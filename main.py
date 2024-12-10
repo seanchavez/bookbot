5e1471a2d021b1596243bf92d59ebf1f8d3965b8
seanchavez <realseanchavez@gmail.com>
2024-12-09 19:04:06 -0800

    implement count_words

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     book_text = get_file_contents("books/frankenstein.txt")
-    print(book_text)
+    words = count_words(book_text)
+    print(count_words(book_text))
 
 def get_file_contents(path):
     with open(path) as f:
         return f.read()
+    
+def count_words(str):
+    return len(str.split())
 
 main()
