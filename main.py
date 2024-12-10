3730f4f7fea1c53aab2230a99cdf093b39f10673
seanchavez <realseanchavez@gmail.com>
2024-12-10 10:06:06 -0800

    implement print_report

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_text = get_file_contents("books/frankenstein.txt")
-    words = count_words(book_text)
-    chars_count = get_chars_count(book_text)
-    print(chars_count)
+    print_report(book_text)
+    # word_count = count_words(book_text)
+    # chars_count = get_chars_count(book_text)
+    # print(chars_count)
 
 def get_file_contents(path):
     with open(path) as f:
@@ -20,5 +21,26 @@ def get_chars_count(str):
         else:
             chars_count[char] = 1
     return chars_count
+
+def make_list_of_dicts(dict):
+    lst = []
+    for k, v in dict.items():
+        if k.isalpha():
+            lst.append({k: v})
+    return lst
+
+def sort_on(dict):
+    return list(dict.values())[0]
+
+
+def print_report(txt):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(txt)} words found in the document\n")
+    char_counts = make_list_of_dicts(get_chars_count(txt))
+    char_counts.sort(reverse=True, key=sort_on)
+    for dict in char_counts:
+        char, count = list(dict.items())[0]
+        print(f"The '{char}' character was found {count} times")
+    print("---End report---")
 
 main()
