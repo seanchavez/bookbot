4a45db07cf8b19b01307d4504ea5740e7ad5f5fb
seanchavez <realseanchavez@gmail.com>
2024-12-09 19:12:06 -0800

    implement get_chars_count

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     book_text = get_file_contents("books/frankenstein.txt")
     words = count_words(book_text)
-    print(count_words(book_text))
+    chars_count = get_chars_count(book_text)
+    print(chars_count)
 
 def get_file_contents(path):
     with open(path) as f:
@@ -9,5 +10,15 @@ def get_file_contents(path):
     
 def count_words(str):
     return len(str.split())
+
+def get_chars_count(str):
+    chars_count = {}
+    for char in str:
+        char = char.lower()
+        if char in chars_count:
+            chars_count[char] += 1
+        else:
+            chars_count[char] = 1
+    return chars_count
 
 main()
